392ba4b1dd670d90240708a074d67b605d6f1d6c
johannes9622 <johannesduo7@gmail.com>
2025-09-18 21:34:33 -0500

    Delete auth.ts

--- auth.ts
+++
@@ -1,1 +0,0 @@
-export function getToken(){ if (typeof window==='undefined') return ''; return localStorage.getItem('token')||''; } export function setToken(t){ if (typeof window==='undefined') return; localStorage.setItem('token', t); } export function clearToken(){ if (typeof window==='undefined') return; localStorage.removeItem('token'); }
