33839c66146b543c686ba1ee6e09e8f1c3e545a9
johannes9622 <johannesduo7@gmail.com>
2025-09-19 00:23:49 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,17 @@
-module.exports={reactStrictMode:true};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.duoaxs.com' }],
+        destination: 'https://duoaxs.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
