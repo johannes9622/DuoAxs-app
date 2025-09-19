e6548a9bdf4d24adc12574a143905c55f0759bb9
johannes9622 <johannesduo7@gmail.com>
2025-09-19 00:34:40 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,9 +4,17 @@ const nextConfig = {
 
   async redirects() {
     return [
+      // Redirect www.duoaxs.com → duoaxs.com
       {
         source: '/:path*',
         has: [{ type: 'host', value: 'www.duoaxs.com' }],
+        destination: 'https://duoaxs.com/:path*',
+        permanent: true,
+      },
+      // Redirect http:// → https://
+      {
+        source: '/:path*',
+        has: [{ type: 'protocol', value: 'http' }],
         destination: 'https://duoaxs.com/:path*',
         permanent: true,
       },
